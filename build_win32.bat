2264b97cdfe39ec97521d7177d05259d4ab27d90
horsicq <horsicq@gmail.com>
2020-03-21 14:57:06 +0100

    Fix

--- build_win32.bat
+++ build_win32.bat
@@ -63,7 +63,8 @@ copy %SOURCE_PATH%\build\release\winloader.exe %SOURCE_PATH%\release\%BUILD_NAME
 copy %SOURCE_PATH%\build\release\pdbripper.exe %SOURCE_PATH%\release\%BUILD_NAME%\base\
 copy %SOURCE_PATH%\build\release\pdbripperc.exe %SOURCE_PATH%\release\%BUILD_NAME%\base\
 
-copy %VS_PATH%\"DIA SDK\bin\msdia120.dll" %SOURCE_PATH%\release\%BUILD_NAME%\base\
+rem copy %VS_PATH%\"DIA SDK\bin\msdia120.dll" %SOURCE_PATH%\release\%BUILD_NAME%\base\
+copy %SOURCE_PATH%\files\msdia140.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
 
 copy %QT_PATH%\bin\Qt5Widgets.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
 copy %QT_PATH%\bin\Qt5Gui.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
