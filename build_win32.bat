f8d6eaede17e4f2e44733354000ab44219fd6352
horsicq <horsicq@gmail.com>
2021-08-13 14:08:27 +0200

    Fix: 2021-08-13

--- build_win32.bat
+++ build_win32.bat
@@ -1,87 +1,40 @@
-set VS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0"
-set SEVENZIP_PATH="C:\Program Files\7-Zip"
-set QT_PATH="C:\Qt5.6.3\5.6.3\msvc2013"
+set VSVARS_PATH="C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
+set QMAKE_PATH="C:\Qt5.6.3\5.6.3\msvc2013\bin\qmake.exe"
+set SEVENZIP_PATH="C:\Program Files\7-Zip\7z.exe"
 
-set BUILD_NAME=pdbripper_win32_portable
-set CUR_DRIVE=%CD:~0,2%
-set SOURCE_PATH=%~dp0
-mkdir %SOURCE_PATH%\build
-mkdir %SOURCE_PATH%\build\loader
-mkdir %SOURCE_PATH%\release
-set /p RELEASE_VERSION=<%SOURCE_PATH%\release_version.txt
+set X_SOURCE_PATH=%~dp0
+set X_BUILD_NAME=pdbripper
+set X_BUILD_PREFIX=win32
+set /p X_RELEASE_VERSION=<%X_SOURCE_PATH%\release_version.txt
 
-set QT_SPEC=win32-msvc2013
-call %VS_PATH%\VC\bin\vcvars32.bat
-set ZIP_NAME=%BUILD_NAME%_%RELEASE_VERSION%
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %VSVARS_PATH%
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %QMAKE_PATH%
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %SEVENZIP_PATH%
 
-%CUR_DRIVE%
-cd %SOURCE_PATH%
+IF NOT [%X_ERROR%] == [] goto exit
 
-cd %SOURCE_PATH%\gui_source
-%QT_PATH%\bin\qmake.exe gui_source.pro -r -spec %QT_SPEC% "CONFIG+=release"
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_init
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_build %X_SOURCE_PATH%\pdbripper_source.pro
 
-nmake Makefile.Release clean
-nmake
-del Makefile
-del Makefile.Release
-del Makefile.Debug
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %X_SOURCE_PATH%\build\release\pdbripper.exe
 
-cd %SOURCE_PATH%
+IF NOT [%X_ERROR%] == [] goto exit
 
-cd %SOURCE_PATH%\console_source
-%QT_PATH%\bin\qmake.exe console_source.pro -r -spec %QT_SPEC% "CONFIG+=release"
+call %X_SOURCE_PATH%\build_tools\windows.cmd check_file %X_SOURCE_PATH%\build\release\pdbripperc.exe
 
-nmake Makefile.Release clean
-nmake
-del Makefile
-del Makefile.Release
-del Makefile.Debug
+IF NOT [%X_ERROR%] == [] goto exit
 
-cd %SOURCE_PATH%
-cd %SOURCE_PATH%\winloader_source
+copy %X_SOURCE_PATH%\build\release\pdbripper.exe %X_SOURCE_PATH%\release\%X_BUILD_NAME%\
+copy %X_SOURCE_PATH%\build\release\pdbripperc.exe %X_SOURCE_PATH%\release\%X_BUILD_NAME%\
+copy %X_SOURCE_PATH%\files\msdia140.dll %X_SOURCE_PATH%\release\%X_BUILD_NAME%\
 
-Rc.exe /v rsrc.rc
-Cvtres.exe /v rsrc.res
-cl.exe /c winloader.cpp /D_USING_V110_SDK71_ /GS- /Oi-
-link.exe /SUBSYSTEM:WINDOWS winloader.obj rsrc.res user32.lib kernel32.lib shell32.lib /NODEFAULTLIB /SAFESEH:NO /SUBSYSTEM:WINDOWS,5.01 /ENTRY:entry /OUT:%SOURCE_PATH%\build\release\winloader.exe
-if exist winloader.obj del winloader.obj
-if exist rsrc.res del rsrc.res
-if exist rsrc.obj del rsrc.obj
+call %X_SOURCE_PATH%\build_tools\windows.cmd deploy_qt_library Qt5Widgets
+call %X_SOURCE_PATH%\build_tools\windows.cmd deploy_qt_library Qt5Gui
+call %X_SOURCE_PATH%\build_tools\windows.cmd deploy_qt_library Qt5Core
+call %X_SOURCE_PATH%\build_tools\windows.cmd deploy_qt_library Qt5Widgets
+call %X_SOURCE_PATH%\build_tools\windows.cmd deploy_vc_redist
 
-cd %SOURCE_PATH%
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_release
 
-rem rmdir /s /q release\
-mkdir %SOURCE_PATH%\release
-
-rem portable
-rmdir /s /q %SOURCE_PATH%\release\%BUILD_NAME%\
-mkdir %SOURCE_PATH%\release\%BUILD_NAME%
-mkdir %SOURCE_PATH%\release\%BUILD_NAME%\base
-mkdir %SOURCE_PATH%\release\%BUILD_NAME%\base\platforms
-
-copy %SOURCE_PATH%\build\release\winloader.exe %SOURCE_PATH%\release\%BUILD_NAME%\pdbripper.exe
-copy %SOURCE_PATH%\build\release\pdbripper.exe %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %SOURCE_PATH%\build\release\pdbripperc.exe %SOURCE_PATH%\release\%BUILD_NAME%\base\
-
-rem copy %VS_PATH%\"DIA SDK\bin\msdia120.dll" %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %SOURCE_PATH%\files\msdia140.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-
-copy %QT_PATH%\bin\Qt5Widgets.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Gui.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Core.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\bin\Qt5Network.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %QT_PATH%\plugins\platforms\qwindows.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\platforms\
-
-copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcp120.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-copy %VS_PATH%\VC\redist\x86\Microsoft.VC120.CRT\msvcr120.dll %SOURCE_PATH%\release\%BUILD_NAME%\base\
-
-cd %SOURCE_PATH%\release
-if exist %BUILD_NAME%_%RELEASE_VERSION%.zip del %BUILD_NAME%_%RELEASE_VERSION%.zip
-%SEVENZIP_PATH%\7z.exe a %BUILD_NAME%_%RELEASE_VERSION%.zip %BUILD_NAME%\*
-cd %SOURCE_PATH%
-
-cd %SOURCE_PATH%
-
-rmdir /s /q %SOURCE_PATH%\release\%BUILD_NAME%\
-
-goto:eof
+:exit
+call %X_SOURCE_PATH%\build_tools\windows.cmd make_clear
